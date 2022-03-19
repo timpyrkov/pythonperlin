a5d0ba21431a8d429326e123dc6b35d291e6097b
Tim Pyrkov <tim.pyrkov@gmail.com>
2022-03-19 09:27:32 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'pythonperlin'
-copyright = '2022, Tim pyrkov'
+copyright = '2022, Tim Pyrkov'
 author = 'Tim Pyrkov'
 
 # The full version, including alpha/beta/rc tags
